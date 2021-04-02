988eb83e4299d9de3d6ce5b5b2dbe05bdce419ed
Ayoush Moktan Lama <aayushlama26@gmail.com>
2021-04-02 16:58:36 +0545

    'db;

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Mongoose = require('mongoose');
 
 
-Mongoose.connect(`${process.env.DB_URL}/${process.env.DB_NAME}`,{
+Mongoose.connect(`${process.env.DB_URL}`,{
     useUnifiedTopology: true,
     useNewUrlParser: true
 },(err,done)=>{
